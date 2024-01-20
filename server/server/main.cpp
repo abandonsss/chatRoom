0531d13b7dfb9406bb248892386cc57f257830e7
abandonsss <94227785+abandonsss@users.noreply.github.com>
2024-01-20 21:16:32 +0800

    Update main.cpp

--- server/server/main.cpp
+++ server/server/main.cpp
@@ -15,37 +15,6 @@ void prog_exit(int signo)
     g_reactor.uninit();//关闭服务器
 }
 
-void daemon_run()
-{
-    int pid;
-    signal(SIGCHLD, SIG_IGN);
-    pid = fork();
-    if (pid < 0)
-    {
-        main_logger->error("fork error");
-        exit(-1);
-    }
-    else if (pid > 0) {
-        exit(0);
-    }
-    //之前parent和child运行在同一个session里,parent是会话（session）的领头进程,
-    //parent进程作为会话的领头进程，如果exit结束执行的话，那么子进程会成为孤儿进程，并被init收养。
-    //执行setsid()之后,child将重新获得一个新的会话(session)id。
-    //这时parent退出之后,将不会影响到child了。
-    setsid();
-    int fd;
-    fd = open("/dev/null", O_RDWR, 0);
-    if (fd != -1)
-    {
-        dup2(fd, STDIN_FILENO);
-        dup2(fd, STDOUT_FILENO);
-        dup2(fd, STDERR_FILENO);
-    }
-    if (fd > 2)
-        close(fd);
-}
-
-
 int main(int argc, char* argv[])
 {
     //设置信号处理
@@ -55,28 +24,7 @@ int main(int argc, char* argv[])
     signal(SIGKILL, prog_exit);
     signal(SIGTERM, prog_exit);
 
-    short port = 0;
-    int ch;
-    bool bdaemon = false;
-    while ((ch = getopt(argc, argv, "p:d")) != -1)
-    {
-        switch (ch)
-        {
-            case 'd':
-                bdaemon = true;
-                break;
-            case 'p':
-                port = atol(optarg);
-                break;
-        }
-    }
-
-    if (bdaemon)
-        daemon_run();
-
-
-    if (port == 0)
-        port = 12345;
+    short port = 12345;
 
     if (!g_reactor.init("0.0.0.0", 12345))
         return -1;
